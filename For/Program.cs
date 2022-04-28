9fab0af47995376618952a0a6cee69ab769e8f98
ksunia-nia <ksunia-nia@yandex.ru>
2022-04-28 23:33:29 +0300

    For

---
+++ For/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string Method4(int count,string c)
+{
+        string result = String.Empty;// String.Empty пустая строка
+
+        
+    for ( int i =0;  i < count; i++)
+    {
+        result = result + c;
+     }
+    return result;
+}
+
+string res = Method4(5," подозрительно тихо(( ");
+Console.WriteLine(res);
